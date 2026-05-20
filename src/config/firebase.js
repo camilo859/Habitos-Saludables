4b10ebf99589dc9e3af6a606796cd486d3ad710b
Alvaro Escobar <aacastillo@estudiante.uniajc.edu.co>
2026-05-20 15:17:51 -0500

    fix: compatibilidad Firebase Auth para Expo Web

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,20 @@
-// src/config/firebase.js
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+
+import {
+  getFirestore,
+} from 'firebase/firestore';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Tu configuración de Firebase
+import { Platform } from 'react-native';
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBahbKsLWnh_T7NDjWhUYek3NIgEgPf9M",
   authDomain: "habitsapp-569c1.firebaseapp.com",
@@ -14,15 +24,28 @@ const firebaseConfig = {
   appId: "1:846662635029:web:692858320ab178d590081c"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar Auth con persistencia para React Native
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-});
+let auth;
 
-// Inicializar Firestore
+// WEB
+if (Platform.OS === 'web') {
+
+  auth = getAuth(app);
+
+} else {
+
+  // ANDROID / IOS
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(
+      AsyncStorage
+    ),
+  });
+
+}
+
 const db = getFirestore(app);
 
 export { auth, db };
+
+export default app;
